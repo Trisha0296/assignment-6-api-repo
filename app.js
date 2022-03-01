2cf0f34847a3e20cc2aa139354b172930ef25c70
Trisha0296 <trishah0296@gmail.com>
2022-03-01 22:22:56 +0600

    addad the full part

--- app.js
+++ app.js
@@ -1,53 +1,59 @@
 const allPhones = () => {
-  document.getElementById('phone-container').innerHTML = " ";
+  // document.getElementById('phone-container').innerHTML = " ";
+
+
   const searchValu = document.getElementById('search-box').value;
+
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchValu}`
-  // console.log(url)
   fetch(url)
     .then(response => response.json())
-    .then((data) => showphoneDetails(data.data));
+    .then((data) => showphoneDetails(data.data.slice(0, 20)));
 
+  document.getElementById('search-box').value = ''
 };
 
 const showphoneDetails = (phones) => {
-
-  for (const phone of phones) {
-    const parent = document.getElementById('phone-container');
+  const parent = document.getElementById('phone-container');
+  parent.textContent = ''
+  phones.forEach(phone => {
     const div = document.createElement('div');
     div.innerHTML = `
-  <div class="cards card border">
-            <div class="pro-pick">
-              <img class="w-30 m-10" src="${phone.image}" alt="">
+          <div class="card p-4 text-center ">
+             
+                <img class="img-fluid" src="${phone.image}" alt="">
+             
+              <h5 class='pt-4'>Name : ${phone.phone_name} </h5>
+              <h6>Phone Brand : ${phone.brand} </h6>
+             
+              <div class="py-3">
+                <button onclick=" phonedetails ('${phone.slug}')" class="btn btn-dark  text-white roundedss">Explore Details</button>
+              </div>
             </div>
-            <h2>Name : ${phone.phone_name} </h2>
-            <h4>Phone Brand : ${phone.brand} </h4>
-           
-            <div class="button">
-              <button onclick=" phonedetails ('${phone.slug}')" class="btn btn-dark  text-white roundedss">Explore Details</button>
-            </div>
-          </div>
- `
+   `
 
-    parent.appendChild(div);
-    // console.log(phone);
+    parent.appendChild(div)
 
-  }
+  });
+
 };
 
 /* Explore details */
 const phonedetails = (id) => {
-  const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => mainFeatures(data.data));
 };
 /*  Explore details call*/
 const mainFeatures = (infoDetails) => {
-  // console.log(infoDetails)
 
-  document.getElementById('details-container').innerHTML = `
-  <div class="cardsDetails card border">
-  <img class="p-10" src="${infoDetails.image}" alt="">
+  const parent = document.getElementById('details-container')
+  parent.textContent = ''
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class=" card border p-3 mx-auto my-3 w-50">
+  <img class="img-fluid" src="${infoDetails.image}" alt="">
     <p>Released date : ${infoDetails.releaseDate}</p>
     <p>Storage : ${infoDetails.mainFeatures.storage}</p>
     <p>DisplaySize : ${infoDetails.mainFeatures.displaySize}</p>
@@ -56,4 +62,6 @@ const mainFeatures = (infoDetails) => {
     <a href="https://dnr.wisconsin.gov/topic/hunt/wildwiapp.html">See more</a>
   </div>
  `
+  parent.appendChild(div)
+
 }

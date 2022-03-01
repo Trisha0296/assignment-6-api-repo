db5ebdca1fe773531b350f031b2baa813a103a00
Trisha0296 <trishah0296@gmail.com>
2022-03-01 18:48:49 +0600

    addas delails

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const allPlayers = () => {
+const allPhones = () => {
   const searchValu = document.getElementById('search-box').value;
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchValu}`
   // console.log(url)
@@ -17,15 +17,39 @@ const showphoneDetails = (phones) => {
               <img class="w-30 m-10" src="${phone.image}" alt="">
             </div>
             <h2>Name : ${phone.phone_name} </h2>
-            <h4>Phone Brand :${phone.brand} </h4>
-            <p>Phone slug :  ${phone.slug} </p>
+            <h4>Phone Brand : ${phone.brand} </h4>
+           
             <div class="button">
-              <button class="btn btn-dark  text-white roundedss">Explore</button>
+              <button onclick=" phonedetails ('${phone.slug}')" class="btn btn-dark  text-white roundedss">Explore Details</button>
             </div>
           </div>
  `
     parent.appendChild(div);
-    // console.log(phone)
+    // console.log(phone);
   }
 
+}
+/* Explore details */
+const phonedetails = (id) => {
+  const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => mainFeatures(data.data));
+};
+/*  Explore details call*/
+const mainFeatures = (infoDetails) => {
+  // console.log(infoDetails)
+  document.getElementById('details-container').innerHTML = `
+  <div class="cards card border">
+  <img src="${infoDetails.image}" alt="">
+    <p>Released date : ${infoDetails.releaseDate}</p>
+    <p>Storage : ${infoDetails.storage}</p>
+    <p>DisplaySize : ${infoDetails.displaySize}</p>
+    <p>ChipSet : ${infoDetails.chipSet}</p>
+    <p>memory : ${infoDetails.memory}</p>
+    <p>released date : ${infoDetails.releaseDate}</p>
+    <a href="#">See more</a>
+  </div>
+
+  `
 }

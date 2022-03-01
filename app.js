3681d977b2b12abe02211890789534d659014d36
Trisha0296 <trishah0296@gmail.com>
2022-03-01 13:59:32 +0600

    url aceess

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// alert()
+const allPlayers = () => {
+  const searchValu = document.getElementById('search-box').value;
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValu}`
+  console.log(url)
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => showphoneDetails(data.data));
+  /* iPhone 12 */
+}
+const showphoneDetails = (phones) => {
+  // console.log(phones)
+}
